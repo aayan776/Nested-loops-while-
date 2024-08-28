a709fbf1d71faa1fb43f263dcb4e459b17963980
aayan776 <forcodingproffesion@gmail.com>
2024-08-28 17:21:49 +0600

    Update Nested loop.py

--- Nested loop.py
+++ Nested loop.py
@@ -15,3 +15,34 @@ while i < len(string):
         count += 1
     i += 1
 print(count)
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+print("The prime numbers between", lower, "and", upper, "are: ")
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+num2 = int(input("Enter number: "))
+t = num2
+numLen = 0
+while t > 0:
+    numLen += 1
+    t = int(t / 10)
+if numLen > 4:
+    numLen = int(numLen / 2)
+    chk = 0
+    while num2 > 0:
+        rem = (num2 % 10)
+        if chk == numLen:
+            MidOne = rem
+        elif chk == (numLen - 1):
+            MidTwo = rem
+        num2 = int(num2 / 10)
+        chk = chk + 1
+    product = MidOne * MidTwo
+    print(product)
+else:
+    print("It is not more than a 4 digit number")
